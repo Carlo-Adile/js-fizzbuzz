ee493fc75c43edc5b8049d63e9890ec3b07c30cf
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-21 16:44:23 +0100

    fix FizzBuzz system output

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,30 @@ const fizzbuzzList = document.querySelector('.fizzbuzz_list');
 
 
 for(let x = 1; x < 101; x++){ 
+  let myItem;
   /* console.log(x); */
 
   /* controlla fizz e buzz con il remainder */
-  if(x % 3 == 0){
+  if(x % 3 == 0 && x % 5 == 0){
+    myItem = "FizzBuzz";
+    
+  }
+  else if(x % 3 == 0){
+    myItem = "Fizz";
     console.log("Fizz");
   }
   else if(x % 5 == 0){
+    myItem = "Buzz";
     console.log("Buzz")
   }
-  else if(x % 3 == 0 && x % 5 == 0){
-    console.log("FizzBuzz")
-  }
   else{
+    myItem = x;
     console.log(x);
   }
+
+  console.log("myItem");
   /* aggiungi gli elementi alla lista html */
-  const markupListItem = `<li>${x}</li>`
+  const markupListItem = `<li>${myItem}</li>`
   fizzbuzzList.innerHTML += markupListItem;
 
 }
