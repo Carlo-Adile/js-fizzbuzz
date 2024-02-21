66618201889a51f0a28283710e717db1ad65bfd8
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-21 17:58:57 +0100

    add different style to li fizzbuzz elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,48 @@ const fizzbuzzList = document.querySelector('.fizzbuzz_list');
 
 for(let x = 1; x < 101; x++){ 
   let myItem;
+  let listItem = document.createElement("li");
 
   /* controlla fizz e buzz attraverso il remainder */
   if(x % 3 == 0 && x % 5 == 0){
     myItem = "FizzBuzz";
-    let listItem = document.createElement("li");
-    fizzbuzzList.append(li);
+    listItem.classList.add('box');
+    listItem.classList.add('style1');
   }
   else if(x % 3 == 0){
     myItem = "Fizz";
+    listItem.classList.add('box');
+    listItem.classList.add('style2');
   }
   else if(x % 5 == 0){
     myItem = "Buzz";
+    listItem.classList.add('box');
+    listItem.classList.add('style3');  
   }
   else{
     myItem = x;
+    listItem.classList.add('box');
+    listItem.classList.add('style4');
   }
+
+  listItem.innerHTML = myItem;
+  fizzbuzzList.append(listItem);
   console.log(myItem);
 
-  /* esercizio: aggiungi gli elementi alla lista html */
-  /* const markupListItem = `<li>${myItem}</li>`
-  fizzbuzzList.innerHTML += markupListItem; */
-
-  /* bonus */
-  const markupListItemStyle = `<li class="box">${myItem}</li>`;
-  /* fizzbuzzList.innerHTML += markupListItemStyle; */
-
-  fizzbuzzList.append(markupListItemStyle);
 }
 
 
 
 
+/* 
+listItem.classList.add('box');
+ */
 
+/* 
+let div = document.createElement("div");
+let p = document.createElement("p");
+div.append(p);
+
+console.log(div.childNodes); // NodeList [ <p> ]
+
+*/
