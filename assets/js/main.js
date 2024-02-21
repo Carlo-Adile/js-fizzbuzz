260d259e76ece4b34988e19128b2edacf059271e
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-21 16:48:02 +0100

    minor fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,23 @@
 const fizzbuzzList = document.querySelector('.fizzbuzz_list');
 
-
-
 for(let x = 1; x < 101; x++){ 
   let myItem;
-  /* console.log(x); */
 
-  /* controlla fizz e buzz con il remainder */
+  /* controlla fizz e buzz attraverso il remainder */
   if(x % 3 == 0 && x % 5 == 0){
     myItem = "FizzBuzz";
-    
   }
   else if(x % 3 == 0){
     myItem = "Fizz";
-    console.log("Fizz");
   }
   else if(x % 5 == 0){
     myItem = "Buzz";
-    console.log("Buzz")
   }
   else{
     myItem = x;
-    console.log(x);
   }
-
-  console.log("myItem");
+  console.log(myItem);
+  
   /* aggiungi gli elementi alla lista html */
   const markupListItem = `<li>${myItem}</li>`
   fizzbuzzList.innerHTML += markupListItem;
