cf2fa1b27c1105a68d1d9e405ce0a51a0b237a47
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-21 16:38:58 +0100

    add if condition that uses % to check x

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 const fizzbuzzList = document.querySelector('.fizzbuzz_list');
 
-for(let x = 1; x < 101; x++){ 
-  console.log(x);
 
-  
+
+for(let x = 1; x < 101; x++){ 
+  /* console.log(x); */
+
+  /* controlla fizz e buzz con il remainder */
+  if(x % 3 == 0){
+    console.log("Fizz");
+  }
+  else if(x % 5 == 0){
+    console.log("Buzz")
+  }
+  else if(x % 3 == 0 && x % 5 == 0){
+    console.log("FizzBuzz")
+  }
+  else{
+    console.log(x);
+  }
+  /* aggiungi gli elementi alla lista html */
+  const markupListItem = `<li>${x}</li>`
+  fizzbuzzList.innerHTML += markupListItem;
 
 }
+
